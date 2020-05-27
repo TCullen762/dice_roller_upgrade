2f159ac4269f4b08a49b404fa7e5364cb37d0d79
Timothy Cullen <cullen762@Timothys-MacBook-Air.local>
2020-05-27 15:53:34 -0400

    intial

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log(' link successful')
+
+//TODO:
+//Create an empty array called attempts 
+//add a click event listener for the roll button
+//on click, take the value from the input box using .value and pass it into a variable
+//within the event listener
+//build a math.random function to return a random numnber ror each number of dice.
+//build a loop, run the loop equal to the value of the input box.
+//push the result of the loop into the attempts array
+//add the values of the array and return the result
+//using an If statement return a text result using either .innerHTML or js DOM to return
+//a string " you are screwed" congrats you almost survived ect. 
+//incorporate a reset button using the reset() method 
+//using js DOM print a picture of colin with blue eyes?
+
+
