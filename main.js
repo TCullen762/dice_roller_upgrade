e43c7118e4c46825fb3184c48802c47b5d8276b5
Timothy Cullen <cullen762@Timothys-MacBook-Air.local>
2020-05-27 21:11:05 -0400

    added HTML boiler plate, input box buttons and divs

--- main.js
+++ main.js
@@ -15,3 +15,47 @@ console.log(' link successful')
 //using js DOM print a picture of colin with blue eyes?
 
 
+
+/* NEED:
+conditional logic to return cheeeky collinisms, maybe fill an array with qoutes and use a loop with math.random to return a 
+collinism. Possibly use the js dowm we just learned to print a pic of him and or qoutes- that may be more fun to do
+also experiment with a FOR loop for efficency?
+
+*/
+
+
+
+//let collin= " troll"
+//let gizmo = " well it looks like you survived this time"
+
+let allRolls= 0
+
+const  inputBox  = document.querySelector("#input_box")//selects HTML node
+const  rollButton = document.querySelector("#roll_button")
+        rollButton.addEventListener("click",function(){
+            let numbDice = inputBox.value //takes input from inputBox
+        
+            let counter= 0
+            while( counter< numbDice){
+                let rando = Math.floor(Math.random()*6)+1
+                 allRolls += rando
+                  //attempts.push(rando)
+               
+                  const inputResult =document.querySelector("#inputResult")//selects HTML Node
+                  inputResult.innerHTML = allRolls
+                  counter += 1
+            } //end loop
+            
+            console.log(allRolls)
+
+        })
+
+        /*if(allRolls<=1){
+           
+            let engryVamp = document.querySelector("#sucksForYou")//selects HTML Node
+            engryVamp.innerHTML = gizmo
+            }else{
+                let engryVamp = document.querySelector("#sucksForYou")//selects HTML Node
+            engryVamp.innerHTML = collin
+            }
+        */
